1652c36005634236559d8b8162f6582fb6e1e7a8
unknown <matiasbritez36@gmail.com>
2025-10-19 12:31:44 -0300

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+function Header({ titulo }) {
+  return (
+    <Container className="text-center mt-4 mb-4">
+      <h1>{titulo}</h1>
+      <hr />
+    </Container>
+  );
+}
+
+export default Header;
